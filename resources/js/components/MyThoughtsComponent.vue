ef9f0529c7398300acc35b6366e4975c348958d5
jainner salas <jsalas902@gmail.com>
2019-08-06 01:28:13 -0400

    Crud en el frontend

---
+++ resources/js/components/MyThoughtsComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+            <form-component @new="addThought"></form-component>
+
+            <thought-component 
+                v-for="(thought, index) in thoughts" 
+                :key="thought.id"
+                :thought="thought"
+                @delete="deleteThought(index)"
+                @update="updateThought(index, ...arguments)">                
+            </thought-component>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                thoughts: [{
+                    'id': 1,
+                    'description': 'abc',
+                    'created_at': '03/08/2019'
+                }]
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods: {
+            addThought(thought) {
+                this.thoughts.push(thought);
+            },
+            updateThought(index, thought) {
+                this.thoughts[index] = thought;
+            },
+            deleteThought(index) {
+                this.thoughts.splice(index, 1);
+            }
+        }
+    }
+</script>
---
+++ resources/js/components/ThoughtComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="card mt-4">
+        <div class="card-header">Publicado en {{thought.created_at}}</div>
+
+        <div class="card-body">
+            <input v-if="editMode" type="text" class="form-control" v-model="thought.description">
+            <P v-else>{{thought.description}}</P>
+        </div>
+        <div class="card-footer">
+
+            <button v-if="editMode" class="btn btn-success" v-on:click="onClickUpdate()">
+                Guardar cambios
+            </button>
+            <button v-else class="btn btn-light" v-on:click="onClickEdit()">
+                Editar
+            </button>
+
+            <button class="btn btn-danger" v-on:click="onClickDelete()">
+                Eliminar
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['thought'],
+        data() {
+            return {
+                editMode: false
+             }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods: {
+            onClickEdit() {
+                this.editMode= true;
+            },
+            onClickUpdate() {
+                this.editMode = false;
+                this.$emit('update', thought);
+            },
+            onClickDelete() {
+                this.$emit('delete');
+            }
+        }
+    }
+</script>
